811b4af1546ba031d0679d5b1823c6c5a8ffed49
Sudipta Chakraborty <sudiptachakraborty86170@gmail.com>
2026-07-12 23:48:55 +0530

    Refactor backend structure

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,8 +1,42 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
+import shutil
+import os
 
 router = APIRouter()
 
 
+@router.get("/")
+def home():
+    return{
+        "message": "Welcome to the Backend of AcousticSpace .",
+        "status" : "Running"
+    }
+
+@router.get("/health")
+def health_check_server():
+    return{
+        "status" : "OK"
+    }
+
+@router.get("/about")
+def about():
+    return {
+        "project": "AcousticSpace",
+        "theme": "Deepfake Audio Detection",
+        "backend": "FastAPI",
+        "version": "1.0.0"
+    }
+
+@router.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    file_path = os.path.join("updates",file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {
+        "filename": file.filename,
+        "message": "Audio received successfully",
+        "saved_location": file_path
+    }
 @router.get("/test")
 def test_route():
     return {
--- backend/main.py
+++ backend/main.py
@@ -1,41 +1,47 @@
 from fastapi import FastAPI, UploadFile, File
 import os
 import shutil
+from app.routes import router
+
+
 app = FastAPI(
     title = "AcousticSpace",
     version = "1.0.0",
     description = "Backend API of AcousticSpace Project."
 )
 
-@app.get("/")
-def home():
-    return{
-        "message": "Welcome to the Backend of AcousticSpace .",
-        "status" : "Running"
-    }
+app.include_router(router)
 
-@app.get("/health")
-def health_check_server():
-    return{
-        "status" : "OK"
-    }
 
-@app.get("/about")
-def about():
-    return {
-        "project": "AcousticSpace",
-        "theme": "Deepfake Audio Detection",
-        "backend": "FastAPI",
-        "version": "1.0.0"
-    }
+# @app.get("/")
+# def home():
+#     return{
+#         "message": "Welcome to the Backend of AcousticSpace .",
+#         "status" : "Running"
+#     }
 
-@app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    file_path = os.path.join("updates",file.filename)
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    return {
-        "filename": file.filename,
-        "message": "Audio received successfully",
-        "saved_location": file_path
-    }
+# @app.get("/health")
+# def health_check_server():
+#     return{
+#         "status" : "OK"
+#     }
+
+# @app.get("/about")
+# def about():
+#     return {
+#         "project": "AcousticSpace",
+#         "theme": "Deepfake Audio Detection",
+#         "backend": "FastAPI",
+#         "version": "1.0.0"
+#     }
+
+# @app.post("/predict")
+# async def predict(file: UploadFile = File(...)):
+#     file_path = os.path.join("updates",file.filename)
+#     with open(file_path, "wb") as buffer:
+#         shutil.copyfileobj(file.file, buffer)
+#     return {
+#         "filename": file.filename,
+#         "message": "Audio received successfully",
+#         "saved_location": file_path
+#     }
