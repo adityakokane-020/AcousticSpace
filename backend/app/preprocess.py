b8e872223a5bc20609a56530c46fc27751b57359
Sudipta Chakraborty <sudiptachakraborty86170@gmail.com>
2026-07-15 23:23:02 +0530

    git commit -m "Implement spectrogram feature extraction"

--- backend/app/preprocess.py
+++ backend/app/preprocess.py
@@ -1,10 +1,18 @@
 import librosa
+import numpy as np
 
 def load_audio(file_path):
     audio, sample_rate = librosa.load(file_path, sr=None)
-
     return{
         "sample rate": sample_rate,
         "duration": round(librosa.get_duration(y=audio, sr= sample_rate),2),
         "totaL_samples": len(audio)
+    }
+
+def extract_spectogram(file_path):
+    audio, sample_rate = librosa.load(file_path, sr=None)
+    stft = librosa.stft(audio)
+    spectogram = librosa.amplitude_to_db(np.abs(stft), ref = np.max)
+    return {
+        "spectogram_shape": spectogram.shape
     }
