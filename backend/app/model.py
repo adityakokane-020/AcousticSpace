80245ab11cfa63145648e7270d768970e9555897
Sudipta Chakraborty <sudiptachakraborty86170@gmail.com>
2026-07-25 23:57:20 +0530

    Model loaded to backend

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,7 +1,17 @@
+import torch
+from ml.model import AcousticCNN
+
+MODEL_PATH = "ml/model/best_model.pth"
+
+model = AcousticCNN()
+model.state_dict(
+    model.load( MODEL_PATH, map_location= torch.device("cpu") )
+)
+model.eval()
 def predict_audio(features):
     #ml model details
     return{
-        "prediction": "model is loaded yet",
+        "prediction": "model loaded successfully",
         "confidence": None
 
     }
