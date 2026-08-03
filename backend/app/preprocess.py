d2348c41d34da62354a5ae5b24d7567d543094ef
Sudipta Chakraborty <sudiptachakraborty86170@gmail.com>
2026-08-03 18:45:39 +0530

    Update preprocess.py

--- backend/app/preprocess.py
+++ backend/app/preprocess.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import torch
 from PIL import Image
 from torchvision import transforms
+from scipy.signal import convolve
 
 def load_audio(file_path):
     audio, sample_rate = librosa.load(file_path, sr=None)
@@ -16,10 +17,20 @@ def load_audio(file_path):
 
 def extract_spectrogram(file_path):
     audio, sample_rate = librosa.load(file_path, sr=16000)
+
+    rir = np.zeros(4000)
+    rir[0] = 1.0
+
+    for i in range(1, len(rir)):
+        rir[i] = 0.9 ** i
+
+    audio = convolve(audio, rir, mode="full")
+    audio = audio[:len(audio)]
+
     mel = librosa.feature.melspectrogram(
-        y = audio,
-        sr = sample_rate,
-        n_mels = 128,   #breaking frequency into 128 horizontal rows
+        y=audio,
+        sr=sample_rate,
+        n_mels=128,
     )
 
     mel_db = librosa.power_to_db(mel, ref = np.max)
